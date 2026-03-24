ac7b712b5bc52787c86c7bc7b25e142419a599e0
My Name <132739810+Anshika113@users.noreply.github.com>
2026-03-25 03:45:50 +0530

    render fix

--- backend/app.py
+++ backend/app.py
@@ -141,4 +141,5 @@ def js():
     return send_from_directory(os.path.join(BASE_DIR, "frontend"), "dashboard.js")
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
